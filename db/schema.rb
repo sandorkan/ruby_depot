fba93808d13e193479cc2ff11a67a68a50af5603
unknown <Sandor@.(none)>
2011-05-15 17:27:01 +0200

    update des heroku links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110515142102) do
+ActiveRecord::Schema.define(:version => 20110515143301) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
--- readme.md
+++ readme.md
@@ -2,5 +2,4 @@ Link zur Website
 ----------------
 
 [Sandorkans Depot][depotLink]
-[depotLink]: http://severe-river-762.heroku.com/products
-
+[depotLink]: http://severe-river-762.heroku.com/
